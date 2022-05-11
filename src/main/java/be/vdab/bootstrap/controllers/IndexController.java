aa5f2eb69e2e58e3338b9da845a937294b2858b2
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-11 12:21:40 +0000

    cdn

--- src/main/java/be/vdab/bootstrap/controllers/IndexController.java
+++ src/main/java/be/vdab/bootstrap/controllers/IndexController.java
@@ -1,2 +1,21 @@
-package be.vdab.bootstrap.controllers;public class IndexController {
+package be.vdab.bootstrap.controllers;
+
+import be.vdab.bootstrap.domain.Persoon;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+class IndexController {
+    @GetMapping
+    public ModelAndView index() {
+        return new ModelAndView("index", "personen", new Persoon[] {
+                new Persoon(1, "Eddy", "Wally", "Ertvelde"),
+                new Persoon(2, "Johny", "Turbo", "Kortrijk"),
+                new Persoon(3, "Bobbejaan", "Schoepen", "Turnhout")
+        });
+    }
 }
